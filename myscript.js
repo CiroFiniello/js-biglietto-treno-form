494b5c6ca83e3119cb81a982508d4c552c798534
CodeKiro <kirofiniello@outlook.it>
2024-05-13 02:32:54 +0200

    collegamento degli elementi agli input con seguente funzionamento del pulsante e stampa in console eseguito

--- myscript.js
+++ myscript.js
@@ -1,12 +1,21 @@
-let eta = prompt('Quanti anni hai?');
-let km = prompt ('Quanti chilometri dovrai percorrere?');
-let price = 0.267113;
-if (eta < 21 )
-    {
-    price = (24.552 * eta ) / 100;
-}else 
-if (eta > 63) {
-    price = (37.893 * eta) / 100;
-}
-console.log(price);
-document.getElementById("price").value=price;
+document.querySelector('#cliccami').addEventListener('click', function(){
+
+let kmEl = document.querySelector('input#km');
+let etaEl = document.querySelector('input#eta');
+
+console.log(Number.parseFloat(kmEl.value,10));
+console.log(Number.parseFloat(etaEl.value,10));
+
+});
+// let eta = prompt('Quanti anni hai?');
+// let km = prompt ('Quanti chilometri dovrai percorrere?');
+// let price = 0.267113;
+// if (eta < 21 )
+//     {
+//     price = (24.552 * eta ) / 100;
+// }else 
+// if (eta > 63) {
+//     price = (37.893 * eta) / 100;
+// }
+// console.log(price);
+// document.getElementById("price").value=price;
