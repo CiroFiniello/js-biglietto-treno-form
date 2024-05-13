d252ee5a482f59a3cf3228fb23c4a6e672ca69af
CodeKiro <kirofiniello@outlook.it>
2024-05-13 02:49:22 +0200

    organizzato il calcolo interamente nel blocco della funzione, fine prima parte

--- myscript.js
+++ myscript.js
@@ -5,17 +5,16 @@ let etaEl = document.querySelector('input#eta');
 
 console.log(Number.parseFloat(kmEl.value,10));
 console.log(Number.parseFloat(etaEl.value,10));
-
+    const price = 0.267113;
+    let discount = 0;
+    if ( etaEl.value < 21)
+        {
+            discount = 24.552
+        }
+        else if ( eta > 63 ){
+            discount = 37.893
+        }
+        let totdiscount = (price * kmEl.value) / 100 * discount;
+        let totalprice = (price * kmEl.value) - totdiscount;
+        console.log(totalprice);
 });
-// let eta = prompt('Quanti anni hai?');
-// let km = prompt ('Quanti chilometri dovrai percorrere?');
-// let price = 0.267113;
-// if (eta < 21 )
-//     {
-//     price = (24.552 * eta ) / 100;
-// }else 
-// if (eta > 63) {
-//     price = (37.893 * eta) / 100;
-// }
-// console.log(price);
-// document.getElementById("price").value=price;
